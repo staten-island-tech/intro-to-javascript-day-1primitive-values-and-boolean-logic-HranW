db962b60afac69d6699a1c395ee3c006db297659
HranW <57498766+HranW@users.noreply.github.com>
2020-03-25 17:05:09 -0400

    asdghj

---
+++ index.js
@@ -0,0 +1,44 @@
+let string = "strings";
+let sentence = "Hello " + "Junshen";
+let sentence2 = " and Ema";
+//console.log(sentence + sentence2);
+let student1 = "Sandra";
+let student2 = "Kelly";
+let courseName = "AP Computer Science";
+let teacherName = "Whalen";
+const longSentence = `${student1} and ${student2} are amazing and will be taking ${courseName} with ${teacherName}`; //emplate Literal or Template String
+
+//////////////Number or Integers
+let year = 2020;
+let nextYear = 2020 + 1;
+let past = 2020 / 2;
+let farFuture = 2020 * 50;
+
+//////////Booleans
+/* let trueStatement = true;
+let falseStatement = false; */
+
+//////Null and Undefined
+//let Ron; //declared Ron but not defined Ron
+//console.log(Ron);
+//console.log(Sarah);
+///////////Boolean Logic
+let student = "Caroline";
+let age = 15;
+//console.log(student === "Caroline");
+//console.log(student === "caroline");
+//console.log(age === "15");
+/* console.log(age <= 15 || age > 19);
+console.log(age <= 15 && age > 19); */
+
+////////If statements
+let school = "High Sc";
+if (school === "Elementary") {
+    console.log("You are in k-5");
+} else if (school === "Middle") {
+    console.log("You are in 6,7 or 8th grade");
+} else {
+    console.log("you are in HS");
+}
+let ron = "Ron";
+console.log(ron !== "Ema");
